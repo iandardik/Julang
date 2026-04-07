b145eae88b3f2cc7909bbd352947ca8dbaad5580
Ian Dardik <idardik@GS22421.SP.CS.CMU.EDU>
2026-04-07 12:55:21 -0400

    Updating bool type in Readln lib

--- src/main/kotlin/exspecs/program/library/Readln.kt
+++ src/main/kotlin/exspecs/program/library/Readln.kt
@@ -9,13 +9,13 @@ fun makeReadln() : TransitionSystem {
     val ctx = Context()
     val initState = State(mapOf<Variable,Value>(
         Pair(Variable("input",stringType),Value("", stringType)),
-        Pair(Variable("read",stringType),Value("true", stringType)),
+        Pair(Variable("read", boolType),Value(true, boolType)),
     ))
     val alphabet = setOf(
         SymbolicAction(
             ActionSignature("PromptUser", listOf(Variable("msg", stringType))),
             ctx.mkAnd(
-                ctx.mkEq(ctx.mkStringConst("read"), ctx.mkString("true")),
+                ctx.mkEq(ctx.mkBoolConst("read"), ctx.mkBool(true)),
             ),
             mapOf(),
             Optional.of { state, act ->
@@ -24,7 +24,7 @@ fun makeReadln() : TransitionSystem {
                 State(
                     mapOf(
                         Pair(Variable("input", stringType), Value(input, stringType)),
-                        Pair(Variable("read", stringType), Value("false", stringType)),
+                        Pair(Variable("read", boolType), Value(false, boolType)),
                     )
                 )
             }
@@ -32,10 +32,10 @@ fun makeReadln() : TransitionSystem {
         SymbolicAction(
             ActionSignature("Readln", listOf(Variable("msg", stringType))),
             ctx.mkAnd(
-                ctx.mkEq(ctx.mkStringConst("read"), ctx.mkString("false")),
+                ctx.mkEq(ctx.mkBoolConst("read"), ctx.mkBool(false)),
                 ctx.mkEq(ctx.mkStringConst("msg"), ctx.mkStringConst("input")),
             ),
-            mapOf(Pair(Variable("read", stringType), StringLiteralProgramExpr("true")))
+            mapOf(Pair(Variable("read", boolType), BoolLiteralProgramExpr(true)))
         ),
     )
     // set selfTerminate to false because this is a library function
@@ -46,22 +46,22 @@ fun makeReadlnInt() : TransitionSystem {
     val ctx = Context()
     val initState = State(mapOf(
         Pair(Variable("input", intType),Value(0, intType)),
-        Pair(Variable("read", stringType),Value("true", stringType)),
+        Pair(Variable("read", boolType),Value(true, boolType)),
     ))
     val alphabet = setOf(
         SymbolicAction(
             ActionSignature("PromptUserInt", listOf()),
             ctx.mkAnd(
-                ctx.mkEq(ctx.mkStringConst("read"), ctx.mkString("true")),
+                ctx.mkEq(ctx.mkBoolConst("read"), ctx.mkBool(true)),
             ),
-            mapOf(), //setOf(StringStateVarUpdate(Variable("read","String"), StringUpdateExpr("false"))),
+            mapOf(),
             Optional.of { state, act ->
                 println("Enter an Int:")
                 val input = readln()
                 State(
                     mapOf(
                         Pair(Variable("input", intType), Value(Integer.parseInt(input), intType)),
-                        Pair(Variable("read", stringType), Value("false", stringType)),
+                        Pair(Variable("read", boolType), Value(false, boolType)),
                     )
                 )
             }
@@ -69,10 +69,10 @@ fun makeReadlnInt() : TransitionSystem {
         SymbolicAction(
             ActionSignature("ReadlnInt", listOf(Variable("msg", intType))),
             ctx.mkAnd(
-                ctx.mkEq(ctx.mkStringConst("read"), ctx.mkString("false")),
+                ctx.mkEq(ctx.mkBoolConst("read"), ctx.mkBool(false)),
                 ctx.mkEq(ctx.mkIntConst("msg"), ctx.mkIntConst("input")),
             ),
-            mapOf(Pair(Variable("read", stringType), StringLiteralProgramExpr("true")))
+            mapOf(Pair(Variable("read", boolType), BoolLiteralProgramExpr(true)))
         ),
     )
     // set selfTerminate to false because this is a library function
